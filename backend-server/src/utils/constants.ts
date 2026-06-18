9fd056f89aa0602690dd081ea3a22e1985a08afd
Beelz17a <Beelzebuth17@gmail.com>
2026-06-17 21:10:54 -0500

    Antes del Desastre

--- backend-server/src/utils/constants.ts
+++ backend-server/src/utils/constants.ts
@@ -1,10 +1,10 @@
 import * as path from 'path';
+import { DATA_DIRECTORY } from '../config/env';
 
 export const MIN_PLAYERS = 5;
 export const MAX_PLAYERS = 15;
 
-// Use current working directory to locate data folder when server is started
-export const DATA_DIR = path.join(process.cwd(), 'data');
+export const DATA_DIR = DATA_DIRECTORY;
 export const GAMES_DIR = path.join(DATA_DIR, 'games');
 
 export default { MIN_PLAYERS, MAX_PLAYERS, DATA_DIR, GAMES_DIR };
